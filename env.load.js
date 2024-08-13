56cfce9185868d6ad3865d9d6f9a5c163d432943
pratham704 <pratham.siddannavar28@gmail.com>
2024-08-13 16:37:10 +0530

    refactor: Rename zload.env.js to env.load.js and update loadEnv function

--- env.load.js
+++ env.load.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Function to try loading .env from different levels
+// Function to try loading .env from different levels upto 4 levels 
 const loadEnv = () => {
     let envPath;
     for (let i = 0; i <= 4; i++) {
